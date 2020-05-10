e82502f4688635dc70c357be81375c05ec0132f2
Ioannis Boutsikas <mgioanni@outlook.com>
2020-05-10 14:13:11 -0400

    Add ImGui helper for changing the mesh

--- Sandbox/src/ImGui/ImGuiHelpers.cpp
+++ Sandbox/src/ImGui/ImGuiHelpers.cpp
@@ -19,3 +19,28 @@ void ImGui::MaterialControl(Hazel::HMaterial* material)
 	ImGui::InputFloat("Glossiness", &material->Glossines);
 	ImGui::ColorEdit4("Material Color", &material->Color.x);
 }
+
+void ImGui::MeshSelectControl(Hazel::Ref<Hazel::GameObject>& target, std::vector<Hazel::Ref<Hazel::GameObject>>& models)
+{
+	Hazel::Ref<Hazel::HMesh> selectedMesh = target->Mesh;
+	if (ImGui::BeginCombo("##combo", target->Name.c_str()))
+	{
+		for (auto& m : models) 
+		{
+			bool is_selected = selectedMesh == m->Mesh;
+
+			if (ImGui::Selectable(m->Name.c_str(), is_selected))
+			{
+				selectedMesh = m->Mesh;
+			}
+
+			if (is_selected)
+				ImGui::SetItemDefaultFocus();
+		}
+		ImGui::EndCombo();
+	}
+
+	if (selectedMesh != target->Mesh) {
+		target->Mesh = selectedMesh;
+	}
+}
--- Sandbox/src/ImGui/ImGuiHelpers.h
+++ Sandbox/src/ImGui/ImGuiHelpers.h
@@ -1,9 +1,9 @@
 #pragma once
-
+#include <vector>
 #include "Hazel/ComponentSystem/Transform.h"
 #include "Hazel/ComponentSystem/GameObject.h"
 namespace ImGui {
 	void TransformControl(Hazel::HTransform* transform);
 	void MaterialControl(Hazel::HMaterial* material);
-
+	void MeshSelectControl(Hazel::Ref<Hazel::GameObject>& target, std::vector<Hazel::Ref<Hazel::GameObject>>& models);
 };
